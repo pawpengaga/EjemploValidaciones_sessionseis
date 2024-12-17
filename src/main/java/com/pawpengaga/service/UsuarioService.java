4983e88d1a3b977b5d2730263340b9d2cada509b
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-17 20:10:21 -0300

    service casi listo

---
+++ src/main/java/com/pawpengaga/service/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.pawpengaga.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pawpengaga.model.Usuario;
+import com.pawpengaga.model.UsuarioRequest;
+import com.pawpengaga.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+  @Autowired
+  private UsuarioRepository userRepo;
+
+  // Ahora registramos dentro de UsuarioRequest
+
+  /*
+   * Ahora registramos siempre pasando los datos por lo declarado en request
+   */
+  public Usuario registrarUsuario(UsuarioRequest request){
+    
+    Usuario user = new Usuario();
+    user.setNombre(request.getNombre());
+    user.setClave(request.getClave());
+    user.setCorreo(request.getCorreo());
+
+    return userRepo.save(user);
+
+  }
+
+}
