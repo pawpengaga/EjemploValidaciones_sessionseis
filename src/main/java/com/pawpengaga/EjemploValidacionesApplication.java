e39da82710e314afd6b9d86ce80a8d8fd46d4bfe
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-17 19:51:23 -0300

    primer commit

---
+++ src/main/java/com/pawpengaga/EjemploValidacionesApplication.java
@@ -0,0 +1,13 @@
+package com.pawpengaga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploValidacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploValidacionesApplication.class, args);
+	}
+
+}
