4f90095d00d7a2625de632cbc9669379c456292b
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-17 21:33:53 -0300

    smol fixes

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Prueba de validaciones
 ## Investigar
 
 - Si quisieramos hacer validaciones en base a una expresión regular se debería hacer en una clase a parte...
+
+## Anotaciones de validaciones Java
+
+Ver [este artículo](https://rodriguezizquierdo.com/guia-completa-para-implementar-validaciones-en-java-con-anotaciones-y-grupos-de-restricciones/)
--- src/main/java/com/pawpengaga/controller/UsuarioController.java
+++ src/main/java/com/pawpengaga/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,11 +32,18 @@ public class UsuarioController {
   // Recibimos el UsuarioRequest de arriba
   // @Valid llama automáticamente a @ControllerAdvise en el GlobalExceptionHandler
   @PostMapping("/registro")
-  public String grabaUsuario(@Valid @ModelAttribute("usuarioRequest") UsuarioRequest request, BindingResult result, Model model){
-    Usuario user = userService.registrarUsuario(request);
-    if (result.hasErrors()) return "registro"; // Aqui se capturan errores
-    model.addAttribute("mensaje", "Usuario registrado! Bienvenid@: " + user.getNombre());
-    return "resultado";
+  public String grabaUsuario(@Valid @ModelAttribute("usuarioRequest") UsuarioRequest request, BindingResult result, /* Errors error */ Model model){
+    
+    // Aqui se capturan errores
+    if (result.hasErrors()) {
+      return "registro";
+    } else {
+      Usuario user = userService.registrarUsuario(request);
+      model.addAttribute("mensaje", "Usuario registrado! Bienvenid@: " + user.getNombre());
+      return "resultado";
+    } 
+    // if (error.hasErrors()) return "registro"; // Este es lo mismo y mas claro
+  
   }
 
 }
