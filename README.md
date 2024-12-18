cf57387865849b9a3622ee2822795b58811cae11
Erick Rivera <yomellamoerick.771@gmail.com>
2024-12-17 21:50:21 -0300

    pendientes y regex

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Prueba de validaciones
 
 - Si quisieramos hacer validaciones en base a una expresión regular se debería hacer en una clase a parte...
 
+## Pendientes
+- Hacer una **anotación personalizada** con el método de cálculo del `dígito verificador`
+- Cambiar la validación de una contraseña por una realmente útil
+
 ## Anotaciones de validaciones Java
 
 Ver [este artículo](https://rodriguezizquierdo.com/guia-completa-para-implementar-validaciones-en-java-con-anotaciones-y-grupos-de-restricciones/)
--- src/main/java/com/pawpengaga/model/UsuarioRequest.java
+++ src/main/java/com/pawpengaga/model/UsuarioRequest.java
@@ -2,6 +2,7 @@ package com.pawpengaga.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -17,7 +18,9 @@ public class UsuarioRequest {
   private String nombre;
 
   @NotBlank(message = "La contraseña no puede estar vacia")
+  // Hacer una anotación personalizada con el método de cálculo del dígito verificador
   @Size(min = 6, max = 20, message = "La contraseña debe tener entre 6 y 20 caracteres")
+  @Pattern(regexp = "^(\\d{1,3}(?:\\.\\d{1,3}){2}-[\\dkK])$", message = "La contraseña debe estar formateada como un RUT Chileno... CAMBIAR LUEGO")
   private String clave; // Si quisieramos hacer validaciones en base a una expresión regular se debería hacer en una clase a parte...
   
   @NotBlank(message = "El correo no puede estar vacio...")
